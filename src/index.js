6dbc220fab546b50bbe3564d8079ef2830c7f756
LostLord07 <satishsingh45264209@gmail.com>
2024-03-19 17:53:28 +0530

    api handlers:: Error and Success

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import dotenv from 'dotenv';    
+import dotenv from "dotenv";
+import app from "./app.js";
 dotenv.config({
-    path: './.env'
-})
+  path: "../.env",
+});
 
-import connectDB from './db/index.js';
+import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    
+    app.on("error", (err) => {
+      console.log("Error in server connection: ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed: ", err);
+  });
